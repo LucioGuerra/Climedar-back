96d94424b12bbf6817b86cdcc1bb925246a8bbe8
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-21 20:10:27 -0300

    Creacion del servicio medical-service-sv y su dockerfile

---
+++ medical-service-sv/src/main/java/com/climedar/medical_service_sv/MedicalServiceSvApplication.java
@@ -0,0 +1,13 @@
+package com.climedar.medical_service_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalServiceSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalServiceSvApplication.class, args);
+	}
+
+}
---
+++ medical-service-sv/src/test/java/com/climedar/medical_service_sv/MedicalServiceSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climedar.medical_service_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalServiceSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
