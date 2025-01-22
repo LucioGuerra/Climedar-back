89b48456a38aeca1296ecfca7844be3e686a51ef
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-22 11:09:09 -0300

    Creacion del servicio notification con su configurarion y su dockerfile

---
+++ notification-sv/src/main/java/com/climedar/notification_sv/NotificationSvApplication.java
@@ -0,0 +1,13 @@
+package com.climedar.notification_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSvApplication.class, args);
+	}
+
+}
---
+++ notification-sv/src/test/java/com/climedar/notification_sv/NotificationSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climedar.notification_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
