0db174c3a7967179b02b2cb5b0def3fdeee702c3
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-22 10:30:05 -0300

    Creacion del servicio patient-sv con su configuracion basica y su dockerfile

---
+++ patient-sv/src/main/java/com/climedar/patient_sv/PatientSvApplication.java
@@ -0,0 +1,13 @@
+package com.climedar.patient_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientSvApplication.class, args);
+	}
+
+}
---
+++ patient-sv/src/test/java/com/climedar/patient_sv/PatientSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climedar.patient_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
