4aa0031ac08699667133899c2dab8a23e7516efe
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-22 12:04:06 -0300

    Creaciond el servicio payment con su dockerfile y su configuracion

---
+++ payment-sv/src/test/java/com/climedar/payment_sv/PaymentSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climedar.payment_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
