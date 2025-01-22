c99b35ea6e64d2cb2cfe2c4c8ff92778d93a2cf9
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-22 12:10:26 -0300

    Creacion del servicio consultation, su configuracion y su dockerfile

---
+++ consultation-sv/src/test/java/com/climedar/consultation_sv/ConsultationSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climedar.consultation_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
