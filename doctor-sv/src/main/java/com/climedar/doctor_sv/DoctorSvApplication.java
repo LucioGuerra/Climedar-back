a41532dc0bd82f016bb929b7d0236f1b4b0bd9bb
LucioGuerra <lucioguerra2001@gmail.com>
2025-01-22 10:48:18 -0300

    Creacion del servicio doctor con su configuracion y su dockerfile

---
+++ doctor-sv/src/main/java/com/climedar/doctor_sv/DoctorSvApplication.java
@@ -0,0 +1,13 @@
+package com.climedar.doctor_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorSvApplication.class, args);
+	}
+
+}
---
+++ doctor-sv/src/test/java/com/climedar/doctor_sv/DoctorSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climedar.doctor_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
