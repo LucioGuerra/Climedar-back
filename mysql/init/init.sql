7926202d489430234ede0a1ab6545ace6b555dee
LucioGuerra <lucioguerra2001@gmail.com>
2025-02-09 20:17:43 -0300

    Creacion del docker compose

---
+++ mysql/init/init.sql
@@ -0,0 +1,18 @@
+
+-- Creaa base de datos para consultation-sv
+CREATE DATABASE IF NOT EXISTS consultation_db;
+
+-- Crea la base de datos para doctor-sv
+CREATE DATABASE IF NOT EXISTS doctor_db;
+
+-- Crea la base de datos para medical-service-sv
+CREATE DATABASE IF NOT EXISTS medical_service_db;
+
+-- Crea la base de datos para patient-sv
+CREATE DATABASE IF NOT EXISTS patient_db;
+
+-- Crea la base de datos para payment-sv
+CREATE DATABASE IF NOT EXISTS payment_db;
+
+-- Crea la base de datos para person-sv
+CREATE DATABASE IF NOT EXISTS person_db;
