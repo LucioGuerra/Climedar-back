3828375c3d4cdbb2620f60168656515d8b533d23
LucioGuerra <lucioguerra2001@gmail.com>
2025-02-17 20:49:11 -0300

    Hot fix en la modificacion de medicalSecure y Speciality para ognorar campos nulos

--- patient-sv/src/main/java/com/climedar/patient_sv/mapper/MedicalSecureMapper.java
+++ patient-sv/src/main/java/com/climedar/patient_sv/mapper/MedicalSecureMapper.java
@@ -2,6 +2,7 @@ package com.climedar.patient_sv.mapper;
 
 import com.climedar.patient_sv.entity.MedicalSecure;
 import com.climedar.patient_sv.model.MedicalSecureModel;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
@@ -14,5 +15,6 @@ public interface MedicalSecureMapper {
     MedicalSecureModel toModel(MedicalSecure medicalSecure);
 
     @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = org.mapstruct.NullValuePropertyMappingStrategy.IGNORE)
     void updateEntity(@MappingTarget MedicalSecure medicalSecure, MedicalSecureModel medicalSecureModel);
 }
